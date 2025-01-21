85866f854e3afb5c80455896e22b74484d5d87c4
Luka <alutza94@gmail.com>
2025-01-21 17:01:22 +0100

    product card & shadcn card

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProductCard from "./product-card";
 
 const ProductList = ({
   data,
@@ -16,8 +17,8 @@ const ProductList = ({
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((item, id) => (
-            <div key={id}>{item.name}</div>
+          {limitedData.map((item) => (
+            <ProductCard key={item.slug} product={item} />
           ))}
         </div>
       ) : (
