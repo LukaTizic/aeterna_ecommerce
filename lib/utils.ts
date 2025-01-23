c93f5a4f5982834ec62bda48d5bda72859bc1aa0
Luka <alutza94@gmail.com>
2025-01-23 17:53:40 +0100

    function that converts from prisma object to js object

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+//Convert to regular js objectž
+export function convertToObject<T>(value: T) {
+  return JSON.parse(JSON.stringify(value));
 }
