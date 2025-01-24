a5b2b54b6a1e2505226715791e4f3d61b275e108
Luka <alutza94@gmail.com>
2025-01-24 13:48:25 +0100

    function to format price

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,9 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToObject<T>(value: T) {
   return JSON.parse(JSON.stringify(value));
 }
+
+// format price - decimal
+export function formatNumberDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+}
