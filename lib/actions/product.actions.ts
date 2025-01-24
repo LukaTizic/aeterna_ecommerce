0c45139014377bc1b3f61f68d459c03284d14401
Luka <alutza94@gmail.com>
2025-01-24 16:31:30 +0100

    getProductBySlug action

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,11 +2,19 @@
 import { prisma } from "@/db/prisma";
 import { convertToObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
-// Latest products
+
+// get latest products
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
   return convertToObject(data);
+}
+
+//  get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
