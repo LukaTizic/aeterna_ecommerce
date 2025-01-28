0ccd841d94930cc3bd70917c99d4dbf81f4d77f6
Luka <alutza94@gmail.com>
2025-01-28 18:40:40 +0100

    callback URL redirect

--- app/(auth)/sign-in/credentials-sign-in-form.tsx
+++ app/(auth)/sign-in/credentials-sign-in-form.tsx
@@ -8,12 +8,16 @@ import Link from "next/link";
 import { useActionState } from "react";
 import { useFormStatus } from "react-dom";
 import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useSearchParams } from "next/navigation";
 
 const CredentialsSignInForm = () => {
   const [data, action] = useActionState(signInWithCredentials, {
     success: false,
     message: "",
   });
+
+  const searchParams = useSearchParams();
+  const callBackUrl = searchParams.get("callbackUrl") || "/";
 
   const SignInButton = () => {
     const { pending } = useFormStatus();
@@ -27,6 +31,7 @@ const CredentialsSignInForm = () => {
 
   return (
     <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callBackUrl} />
       <div className="space-y-4">
         <div>
           <Label htmlFor=" email">Email</Label>
