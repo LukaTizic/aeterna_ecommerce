f817cd52b8c57932530b3eae27e38b161da1ee56
Luka <alutza94@gmail.com>
2025-02-10 16:21:18 +0100

    Cart Page

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 export const metadata = {
   title: "Shopping Cart",
 };
 
-const CartPage = () => {
-  return <div>Cart</div>;
+const CartPage = async () => {
+  const cart = await getMyCart();
+
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  );
 };
 
 export default CartPage;
