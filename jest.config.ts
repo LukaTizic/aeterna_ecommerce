1fb969596f734fe402c07c01e81818a33cb33ff8
Luka <alutza94@gmail.com>
2025-02-24 14:35:12 +0100

    jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -103,7 +103,7 @@ const config: Config = {
 
   // A preset that is used as a base for Jest's configuration
   preset: "ts-jest",
-
+  //
   // Run tests from one or more projects
   // projects: undefined,
 
