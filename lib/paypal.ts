9a5a1bcdd62ffac5e02d19199e51a48c2dd1c929
Luka <alutza94@gmail.com>
2025-02-24 20:12:09 +0100

    fixed an error

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -33,7 +33,8 @@ export const paypal = {
     const response = await fetch(url, {
       method: "POST",
       headers: {
-        "Content Type": "application/json",
+        // AAAAAAAAAAA
+        "Content-Type": "application/json",
         Authorization: `Bearer ${accessToken}`,
       },
     });
