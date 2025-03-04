3262d1807fd2dbeba0a179561e1fe5b9b0442a6f
Luka <alutza94@gmail.com>
2025-03-04 15:19:17 +0100

    TableCell color // todo: delete button

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { auth } from "@/auth";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+import { requiredAdmin } from "@/lib/auth-guard";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import Link from "next/link";
+import Pagination from "@/components/shared/pagination";
+import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  title: "Admin Orders",
+};
+
+const AdminOrdersPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  await requiredAdmin();
+  const { page = "1" } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session?.user?.role !== "admin") {
+    throw new Error("User is not authorized");
+  }
+
+  const orders = await getAllOrders({ page: Number(page) });
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-cyan-500">ID</TableHead>
+              <TableHead className="text-cyan-500">DATE</TableHead>
+              <TableHead className="text-cyan-500">TOTAL</TableHead>
+              <TableHead className="text-cyan-500">PAID</TableHead>
+              <TableHead className="text-cyan-500">DELIVERED</TableHead>
+              <TableHead className="text-cyan-500">ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.itemsPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : "Not Paid"}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : "Not Delivered"}
+                </TableCell>
+                <TableCell>
+                  <Button
+                    asChild
+                    variant="outline"
+                    size="sm"
+                    className="text-cyan-500"
+                  >
+                    <Link href={`/order/${order.id}`}>Details</Link>
+                  </Button>
+                  {/* TODO: DELETE BUTTON */}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AdminOrdersPage;
