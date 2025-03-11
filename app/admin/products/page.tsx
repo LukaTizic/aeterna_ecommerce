98fa0bdcb6f84fd7b6febdc4fc0e4e73e030dfef
Luka <alutza94@gmail.com>
2025-03-11 14:44:44 +0100

    get products for admin page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Link from "next/link";
 
-const AdminProductsPage = () => {
-  return <div></div>;
+import { getAllProducts } from "@/lib/actions/product.actions";
+import { formatCurrency, formatId } from "@/lib/utils";
+
+const AdminProductsPage = async (props: {
+  searchParams: Promise<{ page: string; query: string; category: string }>;
+}) => {
+  const searchParams = await props.searchParams;
+
+  const page = Number(searchParams.page) || 1;
+  const searchText = searchParams.query || "";
+  const category = searchParams.category || "";
+
+  const products = await getAllProducts({
+    query: searchText,
+    // page: page,
+    page,
+    // category: category,
+    category,
+  });
+
+  console.log(products);
+
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <h1 className="h2-bold">Products</h1>
+      </div>
+    </div>
+  );
 };
 
 export default AdminProductsPage;
