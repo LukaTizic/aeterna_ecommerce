3d2bcbada248580b277ba56587c973e1238ccf9f
Luka <alutza94@gmail.com>
2025-03-17 12:31:49 +0100

    getAllUsers action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,6 +13,7 @@ import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
 import { z } from "zod";
+import { PAGE_SIZE } from "../constants";
 
 // SIgn in user with credentials
 export async function signInWithCredentials(
@@ -175,4 +176,27 @@ export async function updateProfile(user: { name: string; email: string }) {
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
+}
+
+// Get all ausers
+
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.user.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
