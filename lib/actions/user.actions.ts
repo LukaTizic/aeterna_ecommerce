64435bef9edf4d9fad13048d91357e8bd2693b66
Luka <alutza94@gmail.com>
2025-03-17 12:50:30 +0100

    deleteUser action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -14,6 +14,7 @@ import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
 import { z } from "zod";
 import { PAGE_SIZE } from "../constants";
+import { revalidatePath } from "next/cache";
 
 // SIgn in user with credentials
 export async function signInWithCredentials(
@@ -199,4 +200,17 @@ export async function getAllUsers({
     data,
     totalPages: Math.ceil(dataCount / limit),
   };
+}
+
+// Delete user
+export async function deleteUser(id: string) {
+  try {
+    await prisma.user.delete({ where: { id } });
+
+    revalidatePath("/admin/users");
+
+    return { success: true, message: "User has been deleted." };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
