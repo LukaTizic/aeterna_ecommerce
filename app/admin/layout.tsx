53fdf2af16b11b8ca57823ba9f16d1f1d5ecd00d
Luka <alutza94@gmail.com>
2025-03-18 13:49:20 +0100

    products search

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,6 @@
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-
 import Menu from "@/components/shared/header/menu";
 import MainNav from "./main-nav";
 import AdminSearch from "@/components/admin/admin-search";
@@ -15,16 +14,15 @@ export default function AdminLayout({
     <>
       <div className="flex flex-col">
         <div className="border-b container mx-auto">
-          <div className="flex-center h-16 px-4">
+          <div className="flex items-center h-16 px-4">
             <Link href="/" className="w-22">
               <Image
-                src="/images/logo.png"
+                src="/images/logo.svg"
                 height={48}
                 width={48}
                 alt={APP_NAME}
               />
             </Link>
-            {/* Main Nav */}
             <MainNav className="mx-6" />
             <div className="ml-auto items-center flex space-x-4">
               <AdminSearch />
@@ -32,6 +30,7 @@ export default function AdminLayout({
             </div>
           </div>
         </div>
+
         <div className="flex-1 space-y-4 p-8 pt-6 container mx-auto">
           {children}
         </div>
