adbe89400bd1fa061c1c9ba8296aa8757b30cf23
Luka <alutza94@gmail.com>
2025-04-03 13:17:01 +0200

    resend main function

---
+++ email/purchase-receipt.tsx
@@ -0,0 +1,8 @@
+import { Order } from "@/types";
+import React from "react";
+
+const PurchaseReceiptEmail = ({ order }: { order: Order }) => {
+  return <div>EMAIL HI</div>;
+};
+
+export default PurchaseReceiptEmail;
